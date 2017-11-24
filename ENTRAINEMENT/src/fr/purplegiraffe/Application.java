422bf1160e8de2dcb4a4638c1a05fbfe3e1e11a1
vcoquinot <vcoquinot@gmail.com>
2017-11-24 21:45:12 +0100

    Add files via upload
    base de travail java

---
+++ ENTRAINEMENT/src/fr/purplegiraffe/Application.java
@@ -0,0 +1,13 @@
+package fr.purplegiraffe;
+
+import fr.purplegiraffe.NE_PAS_TOUCHER.ConsoleReader;
+
+public class Application {
+    public void fonctionPrincipale() {
+        dessinerCarre(5, "*");
+    }
+    void dessinerCarre(int taille, String caractere)
+    {
+        
+    }
+}
